8dfc3501998c5d154889f96433aa0614300fc181
liu <1475439461@qq.com>
2021-11-02 19:27:53 +0800

    finish Response part

--- Tools/wordTrans/main.go
+++ Tools/wordTrans/main.go
@@ -13,3 +13,5 @@ func main() {
 
 	log.Printf("name: %s", name)
 }
+
+
--- Web/main.go
+++ Web/main.go
@@ -2,40 +2,63 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+	"net"
+	"os"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request){
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form{
-		fmt.Println("key", k)
-		fmt.Println("value", v)
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
 	}
-	fmt.Fprintf(w, "Hello New man")
+	name := os.Args[1]
+
+	addr := net.ParseIP(name)
+	if addr == nil {
+		fmt.Println("Invalid address")
+	} else {
+		fmt.Println("The address is ", addr.String())
+	}
+	os.Exit(0)
 }
 
-func login(w http.ResponseWriter, r *http.Request){
-		fmt.Println("method:", r.Method)
-		if r.Method == "GET"{
-			t, _ := template.ParseFiles("login.gtpl")
-			log.Println(t.Execute(w, nil))
-		}else{
-			fmt.Println("username:", r.Form[username])
-			fmt.Println("password:", r.Form[password])
-		}
-}
 
-func main(){
-	http.HandleFunc("/", sayHello)
-	http.HandleFunc("/login", login)
-	err := http.ListenAndServe(":9090", nil)
-	if err!=nil{
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"log"
+// 	"net/http"
+// )
+
+// func sayHello(w http.ResponseWriter, r *http.Request){
+// 	r.ParseForm()
+// 	fmt.Println(r.Form)
+// 	fmt.Println("path", r.URL.Path)
+// 	fmt.Println("scheme", r.URL.Scheme)
+// 	fmt.Println(r.Form["url_long"])
+// 	for k, v := range r.Form{
+// 		fmt.Println("key", k)
+// 		fmt.Println("value", v)
+// 	}
+// 	fmt.Fprintf(w, "Hello New man")
+// }
+
+// func login(w http.ResponseWriter, r *http.Request){
+// 		fmt.Println("method:", r.Method)
+// 		if r.Method == "GET"{
+// 			t, _ := template.ParseFiles("login.gtpl")
+// 			log.Println(t.Execute(w, nil))
+// 		}else{
+// 			fmt.Println("username:", r.Form[username])
+// 			fmt.Println("password:", r.Form[password])
+// 		}
+// }
+
+// func main(){
+// 	http.HandleFunc("/", sayHello)
+// 	http.HandleFunc("/login", login)
+// 	err := http.ListenAndServe(":9090", nil)
+// 	if err!=nil{
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+// }
